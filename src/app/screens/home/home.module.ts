a547eadc47bc7d2b38b293b60e76edbf004a8ce0
R.Fawzi <fawzi.rida@hotmail.com>
2021-09-25 01:13:48 +0200

    Feature - add angular material, and decorte home page with a header and footer

--- src/app/screens/home/home.module.ts
+++ src/app/screens/home/home.module.ts
@@ -2,15 +2,19 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { HomeComponent } from "./home.component";
 import { HomeRoutingModule } from "./home.routing.module";
+import {MatIconModule} from "@angular/material/icon"
+import {MatToolbarModule} from "@angular/material/toolbar"
+
 
 
 @NgModule({
-    declarations: [
-      HomeComponent,
-    ],
-    imports: [    
-        CommonModule,
-        HomeRoutingModule],
-    providers: [],
-  })
-  export class AppModule { }
+  declarations: [HomeComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  providers: [],
+})
+export class HomeModule { }
